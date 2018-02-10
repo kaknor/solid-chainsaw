2e155f201ffb7aee273cce7f55b96f88d4c12917
Clyde Caradine <caradi_c@epita.fr>
2018-02-10 01:49:48 +0100

    [SERVER] created delegated ctors

--- src/server.cc
+++ src/server.cc
@@ -5,16 +5,28 @@ Server::Server(const std::string name
 	       , short unsigned int port
 	       , const std::string ip
 	       , const std::string root_dir)
+  : Server(name, port, ip, root_dir, "", false)
+{}
+
+Server::Server(const std::string name
+	       , short unsigned int port
+	       , const std::string ip
+	       , const std::string root_dir
+	       , const std::string log_file
+	       , bool dry_run)
   : name_(name)
   , port_(port)
   , ip_(ip)
   , root_dir_(root_dir)
+  , log_file_(log_file)
+  , dry_run_(dry_run)
 {
   listener_fd_ = socket(AF_INET, SOCK_STREAM, 0);
   if (listener_fd_ < 0)
     std::cerr << "An error occured during socket creation" << std::endl;
 
   int opt = 1;
+  // REUSE allow to reuse socket address's and port's
   if (setsockopt(listener_fd_, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
 		 &opt, sizeof(int)) < 0)
     std::cerr << "An error occured while setting options to the socket" << std::endl;
@@ -32,27 +44,13 @@ Server::Server(const std::string name
   // TODO : change 3 to a private attributes of the priority q len
   if (listen(listener_fd_, 3) == -1)
     std::cerr << "An error occured while listening to the socket" << std::endl;
-
+  // TODO : Maybe create a method accept which will be used
+  // in the main function to be used inside a while(1) loop
   socklen_t address_len = sizeof (address);
   int accepter_fd_ = accept(listener_fd_, (struct sockaddr*) &address, &address_len);
   if (accepter_fd_ < 0)
     std::cerr << "An error occured while accepting the socket" << std::endl;
-
 }
-
-Server::Server(const std::string name
-	       , short unsigned int port
-	       , const std::string ip
-	       , const std::string root_dir
-	       , const std::string log_file
-	       , bool dry_run)
-  : name_(name)
-  , port_(port)
-  , ip_(ip)
-  , root_dir_(root_dir)
-  , log_file_(log_file)
-  , dry_run_(dry_run)
-{}
 
 Server::~Server()
 {
